49c93b0454b4dd51366be9a51a7f63121c80d557
Jun Lee <jun@main-10-230-71-16.tamulink.tamu.edu>
2023-09-14 02:13:56 -0500

    mobile navbar animation applied

--- src/components/utils/Modals/NavbarModal.tsx
+++ src/components/utils/Modals/NavbarModal.tsx
@@ -4,31 +4,47 @@ import { navbarItem } from "../Data/data";
 import { useNavigate } from "react-router-dom";
 
 interface Props {
-  clickMobileMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  handleMobileModal: boolean;
+  setHandleMobileModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const NavbarModal: React.FC<Props> = ({ clickMobileMenu }) => {
+export const NavbarModal: React.FC<Props> = ({
+  handleMobileModal,
+  setHandleMobileModal,
+}) => {
   const navigate = useNavigate();
 
   return (
     <>
       <div>
         <div
-          className="navbar-modal-bg"
+          className={
+            handleMobileModal
+              ? "navbar-modal-bg animated-bg"
+              : "navbar-modal-bg animated-bg-hide"
+          }
           onClick={(e) => {
             const target = document.querySelector(".navbar-modal-bg");
             if (e.target === target) {
-              clickMobileMenu(false);
+              setHandleMobileModal(false);
+              document.body.style.overflow = "unset";
             }
           }}
         >
-          <div className="navbar-modal-container">
+          <div
+            className={
+              handleMobileModal
+                ? "navbar-modal-container animated"
+                : "navbar-modal-container animated-hide"
+            }
+          >
             {navbarItem.map((a, index) => {
               return (
                 <li
                   onClick={() => {
                     navigate(`/${navbarItem[index].toLocaleLowerCase()}`);
-                    clickMobileMenu(false);
+                    setHandleMobileModal(false);
+                    document.body.style.overflow = "unset";
                   }}
                   key={index}
                 >
