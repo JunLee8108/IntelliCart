6adf119fbb12f39a3df6002a52bc885048dbc638
Jun Lee <jun@main-10-230-71-16.tamulink.tamu.edu>
2023-09-16 10:55:12 -0500

    register page and forgot-password page updated

--- src/components/utils/Account/ForgotPassword.tsx
+++ src/components/utils/Account/ForgotPassword.tsx
@@ -3,8 +3,39 @@ import "./ForgotPassword.css";
 export const ForgotPassword: React.FC = () => {
   return (
     <>
-      <div className="forgot-password-container">
-        <h1>ForgotPassword</h1>
+      <div className="forgot-password-container display-flex">
+        <div className="forgot-password-introduction">
+          <p>We will send you an email to update your password.</p>
+        </div>
+        <div className="forgot-password-form">
+          <div className="forgot-password-form-title">
+            <h2>Reset Your Password</h2>
+          </div>
+
+          <form>
+            <input
+              type="email"
+              placeholder="Email*"
+              id="loginEmail"
+              required
+            ></input>
+            <input
+              type="email"
+              placeholder="Confirm Email*"
+              id="loginEmail-confirm"
+              required
+            ></input>
+
+            <div className="forgot-password-button-container display-flex">
+              <div className="forgot-password-button-flexbox display-flex">
+                <button type="reset">RESET</button>
+              </div>
+              <div className="forgot-password-button-flexbox display-flex">
+                <button type="submit">SEND</button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
