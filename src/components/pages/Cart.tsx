299791595d669eb601ca922945bc490b1aa3d96e
JunLee8108 <lejhn1@gmail.com>
2023-10-06 17:07:43 -0500

    cart page first commit

--- src/components/pages/Cart.tsx
+++ src/components/pages/Cart.tsx
@@ -1,32 +1,62 @@
 import "./Cart.css";
+import { LoadingBeforeLogin } from "../utils/Helpers/LoadingBeforeLogIn";
 
-import PropagateLoader from "react-spinners/PropagateLoader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMinus,
+  faPlus,
+  faDeleteLeft,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const Cart: React.FC = () => {
   return (
     <>
+      {/* <LoadingBeforeLogin /> */}
       <div className="cart-container">
-        <h1>Please Sign In First to Use IntelliCart.</h1>
-        <div className="cart-loader-laptop">
-          <PropagateLoader
-            color="black"
-            loading={true}
-            size={18}
-            speedMultiplier={0.6}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
-        </div>
-        <div className="cart-loader-mobile">
-          <PropagateLoader
-            color="black"
-            loading={true}
-            size={12}
-            speedMultiplier={0.6}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
-        </div>
+        <h2 className="cart-header">Your Cart</h2>
+
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <img src="/imgs/jeans.jpg" alt="clothes"></img>
+              </td>
+              <td>
+                Amazon Essentials Boys' Regular Straight-Fit Jeans
+                <div className="cart-color">Blue</div>
+                <div className="cart-size">(Size M)</div>
+              </td>
+              <td>
+                <button className="cart-minusBtn">
+                  <FontAwesomeIcon icon={faMinus} />
+                </button>
+                1
+                <button className="cart-plusBtn">
+                  <FontAwesomeIcon icon={faPlus} />
+                </button>
+              </td>
+              <td>$123</td>
+
+              <td>
+                <button className="cart-deleteBtn">
+                  <FontAwesomeIcon icon={faDeleteLeft} />
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td style={{ width: "0%" }}></td>
+              <td className="cart-total">Your Total:</td>
+              <td>$123</td>
+              <td style={{ width: "20%" }}>
+                <button>
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   );
