7f26e7716c09bd77c96c13945347e85870a9d48f
JunLee8108 <lejhn1@gmail.com>
2023-10-16 12:47:04 -0500

    account submenu added and profile edit and history page added

--- src/components/pages/Profile.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Profile: React.FC = () => {
-  const user = JSON.parse(localStorage.getItem('user') || '{}');
-
-  return(
-    <div>{user ? user.lastName : "Account"}</div>
-  )
-}
