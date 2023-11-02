45dc0ffc0cb927d6fddded4f539d742f32487bca
JunLee8108 <lejhn1@gmail.com>
2023-11-02 18:05:18 -0500

    user image upload page updated

--- src/components/utils/Profile/ProfileUpload.tsx
+++ src/components/utils/Profile/ProfileUpload.tsx
@@ -1,9 +1,21 @@
 import "./ProfileUpload.css";
 
+import { useState } from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRobot } from "@fortawesome/free-solid-svg-icons";
 
 export const ProfileUpload: React.FC = () => {
+  const [image, setImage] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      let img = e.target.files[0];
+      setImage(URL.createObjectURL(img));
+    }
+  };
+
   return (
     <>
       <div className="profile-upload-container">
@@ -16,6 +28,24 @@ export const ProfileUpload: React.FC = () => {
             className="profile-upload-icon"
           />
         </h2>
+
+        <div className="profile-upload-input-container">
+          <input
+            type="file"
+            accept=".png,.jpg"
+            onChange={handleImageChange}
+            id="fileInput"
+          />
+        </div>
+
+        <div className="profile-upload-img-container">
+          {image && (
+            <>
+              <img src={image} alt="Uploaded" className="uploaded-image" />
+              <button className="profile-upload-submit-btn">Submit</button>
+            </>
+          )}
+        </div>
       </div>
     </>
   );
